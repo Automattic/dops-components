3600e4136afc716621c533b39359157a706c61f6
ryelle <kelly.dwan@gmail.com>
2015-08-04 14:36:03 -0400

    Tabs for indentation

--- gulpfile.js
+++ gulpfile.js
@@ -33,53 +33,53 @@ gulp.task("webpack:build", function(callback) {
 });
 
 gulp.task("webpack:build-dev", function(callback) {
-    process.env.NODE_ENV = "production"
+	process.env.NODE_ENV = "production"
 
-    var devAccountConfig = Object.create(getWebpackConfig());
+	var devAccountConfig = Object.create(getWebpackConfig());
 	devAccountConfig.devtool = "sourcemap";
 	devAccountConfig.debug = true;
 
 	// create a single instance of the compiler to allow caching
 	var devCompiler = webpack(devAccountConfig);
 
-    devCompiler.run(function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack:build-dev]", stats.toString({
-            colors: true
-        }));
-        callback();
-    });
+	devCompiler.run(function(err, stats) {
+		if(err) throw new gutil.PluginError("webpack", err);
+		gutil.log("[webpack:build-dev]", stats.toString({
+			colors: true
+		}));
+		callback();
+	});
 });
 
 gulp.task("webpack-dev-server", function(callback) {
-    // Start a webpack-dev-server
-    var serverConfig = Object.create(getWebpackConfig());
-    var entry = {}
-    Object.keys(serverConfig.entry).forEach( function( key ) {
-    	entry[key] = ["webpack/hot/dev-server", serverConfig.entry[key]];
-    });
+	// Start a webpack-dev-server
+	var serverConfig = Object.create(getWebpackConfig());
+	var entry = {}
+	Object.keys(serverConfig.entry).forEach( function( key ) {
+		entry[key] = ["webpack/hot/dev-server", serverConfig.entry[key]];
+	});
 
-    serverConfig.entry = entry;
-    serverConfig.devtool = "sourcemap";//"eval" for performance, but no JSX :(
+	serverConfig.entry = entry;
+	serverConfig.devtool = "sourcemap";//"eval" for performance, but no JSX :(
 	serverConfig.debug = true;
 
 	// console.log(serverConfig.output);
 
-    new WebpackDevServer(webpack(serverConfig), {
-        publicPath: serverConfig.output.publicPath,
-        hot: true,
-        historyApiFallback: true,
-        stats: {
-        	colors: true
-        }
-    }).listen(8085, "localhost", function(err) {
-        if(err) throw new gutil.PluginError("webpack-dev-server", err);
-        // Server listening
-        gutil.log("[webpack-dev-server]", "http://localhost:8085/webpack-dev-server/index.html");
+	new WebpackDevServer(webpack(serverConfig), {
+		publicPath: serverConfig.output.publicPath,
+		hot: true,
+		historyApiFallback: true,
+		stats: {
+			colors: true
+		}
+	}).listen(8085, "localhost", function(err) {
+		if(err) throw new gutil.PluginError("webpack-dev-server", err);
+		// Server listening
+		gutil.log("[webpack-dev-server]", "http://localhost:8085/webpack-dev-server/index.html");
 
-        // keep the server alive or continue?
-        // callback();
-    });
+		// keep the server alive or continue?
+		// callback();
+	});
 });
 
 function getWebpackConfig() {
