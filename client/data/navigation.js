7064af9179aface515b8edb64e84f3f1b3037af9
ryelle <kelly.dwan@gmail.com>
2015-08-12 14:29:04 -0400

    Update demo content

--- client/data/navigation.js
+++ client/data/navigation.js
@@ -4,15 +4,20 @@ module.exports = [{
 		// Display text for the item
 		title: 'Link Item 1',
 		// Path to navigate to
-		url: '#sites',
+		url: '#',
 		// Class name, used to display a specific icon
 		icon: 'briefcase',
 		// Regular expressions for paths that should match this item
 		paths: []
 	},{
 		title: 'Link Item 2',
-		url: '#plans',
+		url: '#',
 		icon: 'moon',
+		paths: []
+	},{
+		title: 'Link Item 3',
+		url: '#',
+		icon: 'leaf',
 		paths: []
 	}]
 }];
