5421f707146524d15ee90431d92b65e92dedc328
Daniel Walmsley <goldsounds@gmail.com>
2015-08-14 23:33:51 -0700

    Update ab test components to meet eslint criteria

--- client/components/ab-test.jsx
+++ client/components/ab-test.jsx
@@ -1,70 +1,70 @@
 // adapted from https://github.com/olahol/react-ab
-var React = require('react');
+var React = require( 'react' );
 
-var random = function() {
+var random = function( ) {
 	try {
-		var arr = new Uint16Array(1);
-		window.crypto.getRandomValues(arr);
+		let arr = new Uint16Array( 1 );
+		window.crypto.getRandomValues( arr );
 		return arr[0] / 65536;
-	} catch (e) {
-		return Math.random();
+	} catch ( e ) {
+		return Math.random( );
 	}
 };
 
 var cookie = {
-	get: function(name) {
+	get: function( name ) {
 		var eq = name + "=",
-			ca = document.cookie.split(";"),
+			ca = document.cookie.split( ";" ),
 			c = null;
-		for (var i = 0; i < ca.length; i += 1) {
+		for ( let i = 0; i < ca.length; i += 1 ) {
 			c = ca[i];
-			while (c.charAt(0) === " ") {
-				c = c.substring(1, c.length);
+			while ( c.charAt( 0 ) === " " ) {
+				c = c.substring( 1, c.length );
 			}
-			if (c.indexOf(eq) === 0) {
-				return decodeURIComponent(c.substring(eq.length, c.length));
+			if ( c.indexOf( eq ) === 0 ) {
+				return decodeURIComponent( c.substring( eq.length, c.length ) );
 			}
 		}
 		return null;
 	},
 
-	set: function(name, value, seconds) {
-		var key = name + "=" + encodeURIComponent(value),
+	set: function( name, value, seconds ) {
+		var key = name + "=" + encodeURIComponent( value ),
 			expires = "",
 			path = "path=/",
 			date = null;
 
-		if (typeof seconds !== "undefined") {
-			date = new Date();
-			date.setTime(date.getTime() + (seconds * 1000));
-			expires = "expires=" + date.toGMTString();
+		if ( typeof seconds !== "undefined" ) {
+			date = new Date( );
+			date.setTime( date.getTime( ) + ( seconds * 1000 ) );
+			expires = "expires=" + date.toGMTString( );
 		}
 
-		document.cookie = [key, expires, path].join(";");
+		document.cookie = [key, expires, path].join( ";" );
 	},
 
-	del: function(name) {
-		this.set(name, "", -1);
+	del: function( name ) {
+		this.set( name, "", -1 );
 	}
 };
 
-var Variant = React.createClass({
+var Variant = React.createClass( {
 	propTypes: {
 		name: React.PropTypes.string.isRequired,
 		children: React.PropTypes.node.isRequired
 	},
 
-	render: function() {
-		if (React.Children.count(this.props.children) === 1) {
+	render: function( ) {
+		if ( React.Children.count( this.props.children ) === 1 ) {
 			return this.props.children;
 		}
 
-		return React.createElement("span", null, this.props.children);
+		return React.createElement( "span", null, this.props.children );
 	}
-});
+} );
 
-var Experiment = React.createClass({
-	getDefaultProps: function() {
+var Experiment = React.createClass( {
+	getDefaultProps: function( ) {
 		return {
 			get: cookie.get,
 			set: cookie.set,
@@ -73,7 +73,7 @@ var Experiment = React.createClass({
 		};
 	},
 
-	getInitialState: function() {
+	getInitialState: function( ) {
 		return {
 			index: null
 		};
@@ -85,57 +85,57 @@ var Experiment = React.createClass({
 		onChoice: React.PropTypes.func.isRequired
 	},
 
-	componentWillMount: function() {
-		var variant = this.props.get(this.cookieName());
+	componentWillMount: function( ) {
+		var variant = this.props.get( this.cookieName() );
 
-		for (var i = 0; i < this.props.children.length; i += 1) {
-			if (variant === this.props.children[i].props.name) {
-				this.setState({
+		for ( let i = 0; i < this.props.children.length; i += 1 ) {
+			if ( variant === this.props.children[i].props.name ) {
+				this.setState( {
 					index: i
-				});
-				this.props.onChoice(this.props.name, this.props.children[i].props.name, i, true);
+				} );
+				this.props.onChoice( this.props.name, this.props.children[i].props.name, i, true );
 				return;
 			}
 		}
 
-		this.chooseVariant();
+		this.chooseVariant( );
 	},
 
-	chooseVariant: function() { //fire
-		var index = Math.floor(this.props.random() * this.props.children.length),
+	chooseVariant: function( ) { //fire
+		var index = Math.floor( this.props.random( ) * this.props.children.length ),
 			variant = this.props.children[index].props.name;
 
-		this.props.set(this.cookieName(), variant);
+		this.props.set( this.cookieName( ), variant );
 
-		this.setState({
+		this.setState( {
 			index: index
-		});
-		this.props.onChoice(this.props.name, variant, index, false);
+		} );
+		this.props.onChoice( this.props.name, variant, index, false );
 
 		return index;
 	},
 
-	getVariant: function() {
+	getVariant: function( ) {
 		var child = this.props.children[this.state.index],
 			variant = child.props.name;
 
 		return variant;
 	},
 
-	cookieName: function() {
+	cookieName: function( ) {
 		return "react_ab_" + this.props.name;
 	},
 
-	clearCookie: function() {
-		this.props.del(this.cookieName());
+	clearCookie: function( ) {
+		this.props.del( this.cookieName( ) );
 	},
 
-	render: function() {
+	render: function( ) {
 		var child = this.props.children[this.state.index];
 
 		return child;
 	}
-});
+} );
 
 module.exports = {
 	Variant: Variant,
--- client/components/tracks-ab-test.jsx
+++ client/components/tracks-ab-test.jsx
@@ -1,44 +1,44 @@
-var React = require('react'),
-	AbTest = require("./ab-test");
+var React = require( 'react' ),
+	AbTest = require( "./ab-test" );
 
 // Simple AbTest wrapper which sends events to Tracks
-var Experiment = React.createClass({
+var Experiment = React.createClass( {
 	propTypes: {
 		name: React.PropTypes.string.isRequired,
 		namespace: React.PropTypes.string.isRequired
 	},
 
-	clearCookie: function() {
-		this.refs.experiment.clearCookie();
+	clearCookie: function( ) {
+		this.refs.experiment.clearCookie( );
 	},
 
-	handleChoice: function(experiment, variant) {
+	handleChoice: function( experiment, variant ) {
 		var _tkq = window._tkq || [];
-		_tkq.push(['recordEvent', this.props.namespace+'_abtest_start',{'abtest_name':experiment,'abtest_variation':variant}]);
+		_tkq.push( ['recordEvent', this.props.namespace+'_abtest_start',{abtest_name:experiment,abtest_variation:variant}] );
 	},
 
-	render: function() {
+	render: function( ) {
 		return (
 			<AbTest.Experiment ref="experiment" onChoice={this.handleChoice} name={this.props.name}>
 				{this.props.children}
 			</AbTest.Experiment>
 		);
 	}
-});
+} );
 
-var Variant = React.createClass({
+var Variant = React.createClass( {
 	propTypes: {
 		name: React.PropTypes.string.isRequired
 	},
 
-	render: function() {
+	render: function( ) {
 		return (
 			<AbTest.Variant name={this.props.name}>
 				{this.props.children}
 			</AbTest.Variant>
 		);
 	}
-});
+} );
 
 module.exports = {
 	Experiment: Experiment,
