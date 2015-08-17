4774ffcf868d1e05fe729b2fbaad3954e5ebd823
Daniel Walmsley <goldsounds@gmail.com>
2015-08-17 12:49:28 -0700

    Add hover-icon component

---
+++ client/components/hover-icon/index.jsx
@@ -0,0 +1,43 @@
+var React = require( 'react' ),
+	Icon = require( '../icon' );
+
+require( './style.scss' );
+
+let HoverIcon = React.createClass( {
+
+	propTypes: {
+		name: React.PropTypes.string.isRequired,
+		type: React.PropTypes.oneOf( ['dark', 'transparent', 'light'] )
+	},
+
+	getDefaultProps: function( ) {
+		return {
+			type: 'dark'
+		};
+	},
+
+	getInitialState: function( ) {
+		return { hover: false };
+	},
+
+	handleMouseOver: function( ) {
+		this.setState( { hover: true } );
+	},
+
+	handleMouseOut: function( ) {
+		this.setState( { hover: false } );
+	},
+
+	render: function( ) {
+		var className = "dops-hovericon dops-hovericon-" + this.props.type;
+		return (
+			<Icon name={this.props.name} style={{position: 'relative'}} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
+				<span className={className}>
+					{this.props.children}
+				</span>
+			</Icon>
+		);
+	}
+} );
+
+module.exports = HoverIcon;
