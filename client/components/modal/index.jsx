9fae2e75f9ffbf5e7bf4136683b127bd5f2ffe57
Daniel Walmsley <goldsounds@gmail.com>
2015-08-31 15:05:16 -0700

    Close modal on escape or click overlay

--- client/components/modal/index.jsx
+++ client/components/modal/index.jsx
@@ -7,6 +7,7 @@ let Modal = React.createClass( {
 	
 	propTypes: {
 		style: React.PropTypes.oneOf( ['wide', 'medium', 'narrow'] ),
+		onRequestClose: React.PropTypes.func
 	},
 
 	getDefaultProps: function() {
@@ -17,10 +18,30 @@ let Modal = React.createClass( {
 
 	componentDidMount: function() {
 		jQuery( 'body' ).addClass( 'dops-modal-showing' );
+		jQuery( document ).keyup( this.handleEscapeKey );
+	},
+
+	handleEscapeKey: function( e ) {
+		console.log( "hit escape" );
+		if ( e.keyCode === 27 ) { // escape key maps to keycode `27`
+			if ( this.props.onRequestClose ) {
+				this.props.onRequestClose();
+			}
+		}
+	},
+
+	handleClickOverlay: function( e ) {
+		e.preventDefault();
+		e.stopPropagation();
+		console.log( "clicked overlay" );
+		if ( this.props.onRequestClose ) {
+			this.props.onRequestClose();
+		}
 	},
 
 	componentWillUnmount: function() {
 		jQuery( 'body' ).removeClass( 'dops-modal-showing' );
+		jQuery( document ).unbind( "keyup", this.handleEscapeKey );
 	},
 
 	render: function() {
@@ -38,7 +59,7 @@ let Modal = React.createClass( {
 		}
 		return (
 			<div>
-				<div className="dops-modal-overlay"></div>
+				<div className="dops-modal-overlay" onClick={this.handleClickOverlay}></div>
 				<div className="dops-modal" style={containerStyle}>
 					{this.props.children}
 				</div>
