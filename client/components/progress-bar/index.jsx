e34de5f82833f5c65c9c7bf5a5a8a18eb848699c
Daniel Walmsley <goldsounds@gmail.com>
2015-10-20 14:57:46 -0700

    Add progress bar component to support Jetpack Onboarding

---
+++ client/components/progress-bar/index.jsx
@@ -0,0 +1,30 @@
+require( './style.scss' );
+
+var React = require('react');
+
+/**
+ * Show progress through the steps
+ * NOTE: progressPercent MUST be rounded to the nearest 10, i.e. 10, 20, 30, 40...100
+ * This is required for the CSS to work (will fix later...)
+ **/
+var ProgressBar = React.createClass({
+
+	propTypes: {
+		style: React.PropTypes.object,
+		progressPercent: React.PropTypes.number.isRequired
+	},
+
+	render: function() {
+		// var classes = 'getting-started__progress progress_'+this.props.progressPercent;
+		return (
+			<div className="dops-progress-bar" style={this.props.style}>
+				<div className="dops-progress-bar__bar">
+					<span className="dops-progress-bar__progress" style={{ width: this.props.progressPercent+'%' }}></span>
+				</div>
+				{this.props.progressPercent}% complete
+			</div>
+		);
+	}
+});
+
+module.exports = ProgressBar;
