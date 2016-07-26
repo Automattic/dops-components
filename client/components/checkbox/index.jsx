be905ac3ce645cd5166ad47c6f47a54bbb7d4107
MichaelArestad <marestad@gmail.com>
2016-07-26 13:20:23 -0400

    Checkbox: added full checkbox component with Calypso styles

---
+++ client/components/checkbox/index.jsx
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' ),
+	classnames = require( 'classnames' ),
+	omit = require( 'lodash/omit' );
+
+require( './style.scss' );
+
+module.exports = React.createClass( {
+
+	displayName: 'Checkbox',
+
+	propTypes: {
+		disabled: React.PropTypes.bool,
+		checked: React.PropTypes.bool
+	},
+
+	getDefaultProps() {
+		return {
+			disabled: false
+		};
+	},
+
+	render: function() {
+		var otherProps = omit( this.props, [ 'className', 'type' ] );
+
+		return (
+			<input { ...otherProps } type="checkbox" className={ classnames( this.props.className, 'dops-checkbox' ) } />
+		);
+	}
+} );
